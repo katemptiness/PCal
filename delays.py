a06ae894ac4c6fd667b14b9c38a0ef1f48b0a267
katemptiness <wellorspell@gmail.com>
2018-05-11 16:00:11 +0300

    delays remade

--- delays.py
+++ delays.py
@@ -1,40 +1,54 @@
 import numpy as np, numpy.linalg as linalg
 import matplotlib.pyplot as plt
 import getopt, sys
+import os
 
 def main():
-    global a, b
+    global files
 
     try:
-	opts, args = getopt.getopt(sys.argv[1:], 'ha:b:', ['aone=', 'btwo='])
+	opts, args = getopt.getopt(sys.argv[1:], 'hf:', ['files='])
     except getopt.GetoptError:
 	sys.exit(2)
     for opt, arg in opts:
 	if opt in ('-h', '--help'):
 	    sys.exit()
-	elif opt in ('-a', '--aone'):
-	    a = arg
-	elif opt in ('-b', '--btwo'):
-	    b = arg
+	elif opt in ('-f', '--files'):
+	    files = arg
 
-def reading(a, b):
-    len1 = sum(1 for line in open(a))
-    len2 = sum(1 for line in open(b))
+def reading(files):
+    all_files = os.listdir(files)
     
-    a = open(a)
-    b = open(b)
+    len_files = len(all_files)
+    
+    k = 0
+    while k < len_files:
+	all_files[k] = files + all_files[k]
+	k = k + 1
     
     li = []
     
-    lenn = min(len1, len2)
-    
     i = 0
-    while i < lenn:
-	li.append(float(a.readline()) - float(b.readline()) * 1e6)
-	i = i + 1
+    while i < len_files:
+	len1 = sum(1 for line in open(all_files[i]))
+	len2 = sum(1 for line in open(all_files[i + 1]))
+	
+	lenn = min(len1, len2)
+	
+	ifile1 = open(all_files[i])
+	ifile2 = open(all_files[i + 1])
+	
+	j = 0
+	while j < lenn:
+	    ch1 = float(ifile1.readline())
+	    ch2 = float(ifile2.readline())
+	    ch = abs(ch1 - ch2) * 1e6
+	    li.append(ch)
+	    j = j + 1
+	
+	i = i + 2
     
-    xlist = np.linspace(1, lenn, lenn)
-
+    xlist = np.linspace(1, lenn * (i / 2), lenn * (i / 2))
     A = (np.vstack([xlist, np.ones(len(xlist))])).transpose()
     m, c = linalg.lstsq(A, li, rcond = -1)[0]
     trend = m * xlist + c
@@ -52,4 +66,4 @@ def reading(a, b):
 
 if __name__ == '__main__':
     main()
-    reading(a, b)
+    reading(files)
