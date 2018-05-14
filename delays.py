a3993bca76a356e46e7f58ff235dc659322d617a
katemptiness <wellorspell@gmail.com>
2018-05-14 17:29:18 +0300

    optimize

--- delays.py
+++ delays.py
@@ -54,84 +54,60 @@ def reading(files):
     i = 0
     k = 0
     while i < len_files:
-    	if i != exception:
-    	    start = ((all_files[i]).find('PCAL_') + 5) + 6
-	    stop = start + 6
+	start = ((all_files[i]).find('PCAL_') + 5) + 6
+	stop = start + 6
 
-	    len1 = sum(1 for line in open(all_files[i]))
-	    len2 = sum(1 for line in open(all_files[i + 1]))
-	    lenn = min(len1, len2)
+	len1 = sum(1 for line in open(all_files[i]))
+	len2 = sum(1 for line in open(all_files[i + 1]))
+	lenn = min(len1, len2)
 
-	    sp.append(float((all_files[i])[start : stop]))
+	sp.append(float((all_files[i])[start : stop]))
 	    
-	    if k > 0:
-	        space = (sp[k] - sp[k - 1]) * 2
+	if k > 0:
+	    space = (sp[k] - sp[k - 1]) * 2
 
+	if i != exception:
 	    ifile1 = open(all_files[i])
 	    ifile2 = open(all_files[i + 1])
 	
 	    li = []
 	    j = 0
 	    while j < lenn:
-	        ch1 = float(ifile1.readline())
-	        ch2 = float(ifile2.readline())
-	        ch = abs(ch1 - ch2) * 1e6
+		ch1 = float(ifile1.readline())
+		ch2 = float(ifile2.readline())
+		ch = abs(ch1 - ch2) * 1e6
 
-	        li.append(ch)
+		li.append(ch)
 
-	        j = j + 1
+		j = j + 1
 	
-	    x2 = x1 + lenn
+	x2 = x1 + lenn
 
-	    if k > 0:
+	if k > 0:
 	
-	        xlist = np.linspace((x1 * 0.5), ((x2 - 1) * 0.5), lenn)
+	    xlist = np.linspace((x1 * 0.5), ((x2 - 1) * 0.5), lenn)
 	
-	        A = (np.vstack([xlist, np.ones(len(xlist))])).transpose()
-	        m, c = linalg.lstsq(A, li, rcond = -1)[0]
-	        trend = m * xlist + c
+	    if i != exception:
+		A = (np.vstack([xlist, np.ones(len(xlist))])).transpose()
+		m, c = linalg.lstsq(A, li, rcond = -1)[0]
+		trend = m * xlist + c
 	    
-	        h = 0
-	        while h < lenn:
-		    ms.append(m)
-		    cs.append(c)
-		    h = h + 1
+	    h = 0
+	    while h < lenn:
+		ms.append(m)
+		cs.append(c)
+		h = h + 1
 
-	        plt.plot(xlist, li, 'o')
-	        plt.plot(xlist, trend)
+	    if i != exception:
+		plt.plot(xlist, li, 'o')
+		plt.plot(xlist, trend)
 	
-	        x1 = x2 + space
+	    x1 = x2 + space
 
-	        h = 0
-	        while h < space:
-		    ms.append(0)
-		    h = h + 1
-
-	else:
-	    start = ((all_files[i]).find('PCAL_') + 5) + 6
-	    stop = start + 6
-
-	    len1 = sum(1 for line in open(all_files[i]))
-	    len2 = sum(1 for line in open(all_files[i + 1]))
-	    lenn = min(len1, len2)
-
-	    sp.append(float((all_files[i])[start : stop]))
-	    
-	    if k > 0:
-	        space = (sp[k] - sp[k - 1]) * 2
-
-	    x2 = x1 + lenn
-
-	    if k > 0:
-	
-	        xlist = np.linspace((x1 * 0.5), ((x2 - 1) * 0.5), lenn)
-	
-	        x1 = x2 + space
-
-	        h = 0
-	        while h < space:
-		    ms.append(0)
-		    h = h + 1
+	    h = 0
+	    while h < space:
+		ms.append(0)
+		h = h + 1
 
 	k = k + 1
 	i = i + 2
