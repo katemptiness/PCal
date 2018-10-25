0d725fb5b87c6b425f24e5eb0647c06396244660
katemptiness <wellorspell@gmail.com>
2018-10-25 12:58:34 +0300

    plot pcal done

--- plots_pcal.py
+++ plots_pcal.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import numpy as np
 import getopt, sys
@@ -6,8 +7,6 @@ import matplotlib.pyplot as plt
 
 def main():
     global ifile
-
-    ifile = None
 
     try:
         opts, args = getopt.getopt(sys.argv[1:], 'hf:', ['ifile='])
@@ -36,13 +35,27 @@ def pcal_plot(ifile):
 	for k in range(q): ifile_massive.append(float((ifile.readline())[0 : -1]))
 	ifile.close()
 
+	plt.figure(1)
+	plt.gcf().canvas.set_window_title(u'Зависимость разности фаз от частоты')
+	plt.plot(ifile_massive, 'o')
+	plt.plot(ifile_massive)
+	plt.grid()
+	plt.xticks(np.arange(0, (len(ifile_massive) + 32), step = 32))
+	plt.xlabel('frequency, MHz')
+	plt.ylabel('phase, grad')
+	plt.show(block = False)
+
 	massive = []
 	for k in range((len(ifile_massive) - 1)): massive.append(ifile_massive[k + 1] - ifile_massive[k])
 
+	plt.figure(2)
+	plt.gcf().canvas.set_window_title(u'Зависимость разности разностных фаз от частоты')
 	plt.plot(massive, 'o')
 	plt.plot(massive)
 	plt.grid()
 	plt.xticks(np.arange(0, (len(ifile_massive) + 32), step = 32))
+	plt.xlabel('frequency, MHz')
+	plt.ylabel('phase, grad')
 	plt.show()
 
 
